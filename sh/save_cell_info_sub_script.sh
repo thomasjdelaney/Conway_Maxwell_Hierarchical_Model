0656e7cd0140c6aa01b0253b565604561072f872
Thomas James Delaney <thomas.delaney@posteo.net>
2020-04-10 19:22:26 +0100

    Added submision script for saving cell info.

---
+++ sh/save_cell_info_sub_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# request resources:
+#PBS -l nodes=1:ppn=16,walltime=01:00:00
+
+export nodes=`cat $PBS_NODEFILE`
+export nnodes=`cat $PBS_NODEFILE | wc -l`
+export confile=inf.$PBS_JOBID.conf
+
+for i in $nodes
+do
+  echo ${i}>>$confile
+done
+
+time python3 py/save_cell_info.py -s
+time python3 py/save_cell_info.py -s -p frontal
