c9c0090f917fc0f75e91a42e9100819feb7c357d
Thomas James Delaney <thomas.delaney@posteo.net>
2020-04-15 20:22:57 +0100

    added fitting functions. Fitting for the comb distn is very slow. That might be a problem.

--- py/loading_script.py
+++ py/loading_script.py
@@ -27,19 +27,36 @@ posterior_dir = os.path.join(proj_dir, 'posterior')
 frontal_dir = os.path.join(proj_dir, 'frontal')
 
 sys.path.append(py_dir)
+sys.path.append(os.path.join(os.environ['PROJ'], 'Conway_Maxwell_Binomial_Distribution'))
 import ConwayMaxwellHierarchicalModel as comh
+import ConwayMaxwellBinomial as comb
 
-def easyLogLikeFit(distn, data, init, bounds):
+def easyLogLikeFit(distn, data, init, bounds, n):
     """
     For fitting a distribution to the data using maximum likelihood method, starting from init.
     Arguments:  distn, the distribution
                 data, the data
                 init, initial guess
                 bounds, (min, max) pairs for each parameter
+                n, the number of cells
     Returns:    fitted distribution object.
     """
-    res = minimize(lambda x,y:-distn.logpmf(data, n=n, a=x, b=y).sum(), init, bounds=bounds)
-    return res.x
+    res = minimize(lambda x:-distn.logpmf(data, n=n, a=x[0], b=x[1]).sum(), init, bounds=bounds)
+    fitted = distn(n=n, a=res.x[0], b=res.x[1])
+    return fitted
+
+def easyCombLogLikeFit(data, init, bounds, n):
+    """
+    For fitting a Conway-Maxwell-binomial distribution using the logpmf function.
+    Arguments:  data,
+                init, initial guess,
+                bounds,
+                n, the number of cells
+    Returns:    fitted distribution object
+    """
+    res = minimize(lambda x:comb.ConwayMaxwellBinomial(x[0], x[1], n).logpmf(data).sum(), init, bounds=bounds)
+    fitted = comb.ConwayMaxwellBinomial(m=n, p=res.x[0], nu=res.x[1])
+    return fitted
 
 if not args.debug:
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Starting main function...')
@@ -61,6 +78,17 @@ if not args.debug:
     fitted_binom = comh.fitBinomialDistn(num_active_cells_binned, total_cells)
     plt.figure(figsize=(5,4))
     comh.plotCompareDataFittedDistn(num_active_cells_binned, fitted_binom)
+    fitted_betabinom = easyLogLikeFit(betabinom, num_active_cells_binned, [1.0, 1.0], [(np.finfo(float).resolution, None), (np.finfo(float).resolution, None)], total_cells)
+    plt.figure(figsize=(5,4))
+    comh.plotCompareDataFittedDistn(num_active_cells_binned, fitted_betabinom)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Fitting Conway-Maxwell-binomial distribution...')
+    fitted_comb = easyCombLogLikeFit(num_active_cells_binned, [0.5, 0], [(0.00001, 1 - 0.00001), (-2,2)], total_cells)
+    # fitted_comb_params = comb.estimateParams(total_cells, num_active_cells_binned, [0.5, 0])
+    # fitted_comb = comb.ConwayMaxwellBinomial(fitted_comb_params[0], fitted_comb_params[1], total_cells)
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Fitted.')
+    plt.figure(figsize=(5,4))
+    comh.plotCompareDataFittedDistn(num_active_cells_binned, fitted_comb)
+
     # plt.show(block=False)
 
 # TODO Beta binomial distribution, multiplicative binomial distribution, conway maxwell binomial distribution
